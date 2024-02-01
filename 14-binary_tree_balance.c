ee42079c60385f06f870ae18bcf7985b1981f09d
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-02-01 20:32:55 +0200

    M

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,36 @@
+#include "binarytrees.h"
+
+/**
+ * binary_tree_heigh - Measures the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL.
+ */
+size_t binary_tree_heigh(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t west , east = 0;
+
+		west = tree->left ? 1 +binary_tree_heigh(tree->left) : 1;
+		east = tree->right ? 1 + binary_tree_heigh(tree->right) :1;
+
+		return ((west > east) ? west : east);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: 0 if tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_heigh(tree->left) - binary_tree_heigh(tree->right));
+	return (0);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binarytrees.h"
+/**
+ * binary_tree_insert_right - Inserts a node as the right-child of a node.
+ *
+ * @parent: A pointer to the node to insert the right-child in.
+ * @value: The value to store in the new node.
+ *
+ * Return: A pointer to the created node, NULL on failure or if parent is NULL.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int vlaue)
+{
+	binary_tree_t *newTree;
+
+	if (!parent)
+		return (NULL);
+
+	newTree = binary_tree_node(parent, value);
+	if (!newTree)
+		return (NULL);
+
+	if (parent_>right)
+	{
+		newTree->right = parent->right;
+		parent->right->parent = newTree;
+	}
+	parent->right = newTree;
+	return (newTree);
+}
