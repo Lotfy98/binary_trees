e1827a121532c12c527c302a9ea9372942c29764
tantohtelmah <tantohtelmah@gmail.com>
2024-01-31 21:49:52 -0500

    task (4,5,8,9,12,13,16,17)

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_perfect - checks if a binary tree is perfect
+* @tree: pointer to the root node of the tree to check
+*
+* Return: 1 if the binary tree is perfect, 0 otherwise
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left_height, right_height, perfect_check1, perfect_check2;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
+	{
+		if (tree->left == NULL || tree->right == NULL)
+		{
+			return (1);
+		}
+		else
+		{
+			perfect_check1 = binary_tree_is_perfect(tree->left);
+			perfect_check2 = binary_tree_is_perfect(tree->right);
+			return (perfect_check1 && perfect_check2);
+		}
+	}
+
+	return (0);
+}
